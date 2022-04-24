55bc1f40f9ffd251d3acf2f705bc93fdda6c9af1
Mohammad Saad <saadsiraj254@gmail.com>
2022-04-25 06:01:51 +0800

    render welcome page example

---
+++ Passenger Counter App/Render Welcome Message/index.js
@@ -0,0 +1,7 @@
+welcomeEl = document.getElementById("welcome-el");
+
+let name = "Saad"
+let greetings = "Welcome to the App";
+welcomeEl.innerText = greetings + " " + name;
+
+welcomeEl.innerText += "👋";
