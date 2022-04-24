d6c3263157d0d6abbb8bc1490aae12835e3102aa
Mohammad Saad <saadsiraj254@gmail.com>
2022-04-25 06:13:13 +0800

    working with strings

---
+++ Examples/stringEx1.js
@@ -0,0 +1,10 @@
+/* create a variable,
+    message -> stores string "page visited by user"
+*/
+
+let username = "user";
+let message = "Page visited by";
+let displayMessage = message + " " + username;
+console.log(displayMessage)
+console.log(message+","+username+"!");
+console.log(message, username,"!"); 
